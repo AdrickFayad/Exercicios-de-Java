462d050d04b0e9f42ef134e0a7675cad39eada7a
Adrick Fayad <adrickfayad96@gmail.com>
2025-11-11 01:52:11 -0300

    Ultimp commit

--- Mod14 Arquivos/course/src/application/Program2.java
+++ Mod14 Arquivos/course/src/application/Program2.java
@@ -3,7 +3,6 @@ package application;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.BufferUnderflowException;
 
 public class Program2 {
 	public static void main(String[] args) {
